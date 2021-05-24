340d7c835cdb1ca7eb32b595316a7650d1aaf378
BrianBravoski <brianbravoski28@gmail.com>
2021-05-24 23:45:50 +0300

    addition of todos component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import './App.css';
+import Todos from './components/Todos'
 
+import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Todo List App</h1>
+      <Todos/>
     </div>
   );
 }
