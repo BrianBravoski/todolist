17d1e1596f60148b1ef2d467dc40762bfe9b492d
BrianBravoski <brianbravoski28@gmail.com>
2021-05-28 14:57:58 +0300

    addition of AddTodo and sme styling

--- src/App.js
+++ src/App.js
@@ -1,56 +1,61 @@
-import React,{Component} from 'react'
-import Header  from './layout/Header'
+import React, { Component } from 'react'
+import Header from './layout/Header'
 import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 
 import './App.css';
 class App extends Component {
- state={
-   todos: [
-     {
-       id:1,
-       title:'Take out the trash',
-       completed:false
-     },
-     {
-       id:2,
-       title:'Dinner with the Mrs',
-       completed:false
-     },
-     {
-       id:3,
-       title:'Brunch meeting with the Boss',
-       completed:false
-     }
-   ]
- }
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with the Mrs',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Brunch meeting with the Boss',
+        completed: false
+      }
+    ]
+  }
 
 
- //toggle complete
-markComplete=(id)=>{
-  this.setState({ todos: this.state.todos.map(todo =>{
-    if(todo.id===id){
-      todo.completed=!todo.completed
-    }
-    return todo;
-  })});
-}
+  //toggle complete
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    });
+  }
 
-//delete a todo
+  //delete a todo
 
-delTodo=(id)=>{
-this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
-   
-}
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
 
-  render(){
-   // console.log(this.state.todos)
-  return (
-    <div className="App">
-      
-      <Header/>
-      <Todos todos={this.state.todos} markComplete={this.markComplete}  delTodo={this.delTodo}/>
-    </div>
-  );
+  }
+
+  render() {
+    // console.log(this.state.todos)
+    return (
+      <div className="App">
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        </div>
+      </div>
+    );
   }
 }
 
